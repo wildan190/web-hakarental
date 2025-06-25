0e07936177f31aa3567ae164d64899d1e8d01a5d
Wildan Belfiore <mikewildan@outlook.com>
2025-06-25 13:31:01 +0700

    update

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://portions-kg-relations-coaching.trycloudflare.com/api",
+  baseURL: "https://ghost-theta-ng-jamaica.trycloudflare.com /api",
 });
 
 export default api;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -18,7 +18,7 @@ export default function MainLayout({ children }) {
 
   const handleLogout = async () => {
     try {
-      await fetch("hhttps://portions-kg-relations-coaching.trycloudflare.com/api/auth/logout", {
+      await fetch("hhttps://ghost-theta-ng-jamaica.trycloudflare.com /api/auth/logout", {
         method: "POST",
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`,
