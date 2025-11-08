658a55742e80df855b2d9d41fcc91b8099b465d8
Wildan Belfiore <mikewildan@outlook.com>
2025-11-09 02:14:12 +0700

    update

---
+++ src/config.js
@@ -0,0 +1,19 @@
+
+const config = {
+  companyName: "Hakarentcar",
+  phoneNumber: "+62XXXXXXXXXX", // Placeholder
+  address: {
+    street: "Jalan Placeholder No. 123",
+    city: "Banjarmasin",
+    region: "Kalimantan Selatan",
+    postalCode: "70123",
+    country: "ID",
+  },
+  socialMedia: {
+    facebook: "https://www.facebook.com/hakarentalmobil",
+    instagram: "https://www.instagram.com/hakarentalmobil",
+    twitter: "",
+  },
+};
+
+export default config;
